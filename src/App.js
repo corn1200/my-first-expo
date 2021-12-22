dd37eff81ee39be5997eee534a19aef0ec6b7698
corn1200 <corn120033@gmail.com>
2021-12-23 00:35:49 +0900

    사용자 정의 컴포넌트 불러오기

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 // 필요한 모듈({..., ...}) 불러오기(import)
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
+import MyButton from './MyButton';
 
 // 화면에 띄워줄 컴포넌트 return()
 export default function App() {
-  console.log('React Native App');
-  return (
-    <View style={styles.container}>
-      <Text>Hi, Kim</Text>
-      <StatusBar style="auto" />
-      <Button title='Button' onPress={() => alert('Click!!')}></Button>
-    </View>
-  );
+    console.log('React Native App');
+    return (
+        <View style={styles.container}>
+            <Text>Hi, Kim</Text>
+            <StatusBar style="auto" />
+            <Button title='Button' onPress={() => alert('Click!!')}></Button>
+            <MyButton />
+        </View>
+    );
 }
 
 // 스타일 시트
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
