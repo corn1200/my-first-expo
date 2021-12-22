e438b60e81b4a4c9dbcb393979fa05d8dbb5307a
corn1200 <corn120033@gmail.com>
2021-12-22 23:40:37 +0900

    앱 실행마다 콘솔 출력

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  console.log('React Native App');
   return (
     <View style={styles.container}>
       <Text>Hi, Kim</Text>
