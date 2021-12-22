b96b29772db6bb5ef1cf37c47be9eab02d913adb
corn1200 <corn120033@gmail.com>
2021-12-22 22:32:30 +0900

    텍스트 색상 변경

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hi, React Native</Text>
+      <Text style={{ color: 'blue' }}>Hi, React Native</Text>
       <StatusBar style="auto" />
     </View>
   );
