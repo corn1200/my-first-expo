0ab17b445a198b83073d18dacc31810d96f928ff
corn1200 <corn120033@gmail.com>
2021-12-23 16:42:00 +0900

    플랫폼에 따라 적용되는 스타일을 다르게 처리할 수 있다

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 // 필요한 모듈({..., ...}) 불러오기(import)
-import { View, Text, TextInput } from 'react-native';
+import { View } from 'react-native';
 import React from 'react';
 import { styles } from './style';
 import { StatusBar } from 'expo-status-bar';
-import Box from './Box';
+import Shadow from './Shadow';
 
 // 화면에 띄워줄 컴포넌트 return()
 export default function App() {
     return (
         <View style={styles.container}>
             <StatusBar style='auto' />
-            <Box style={{ backgroundColor: 'red', height: 100 }} />
-            <Box style={{ backgroundColor: 'green', flex: 1 }} />
-            <Box style={{ backgroundColor: 'blue', height: 100 }} />
+            <Shadow />
         </View>
     );
 }
---
+++ src/Shadow.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { StyleSheet, View, Platform, Text } from "react-native";
+
+const Shadow = () => {
+    return (
+        <View style={styles.Shadow}>
+            <Text>{Platform.OS === 'ios' ? 'ios' : 'android'}</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    Shadow: {
+        backgroundColor: '#e3e3e3',
+        width: 200,
+        height: 200,
+        ...Platform.select(
+            {
+                ios: {
+                    shadowColor: '#000',
+                    shadowOffset: {
+                        width: 10,
+                        height: 10
+                    },
+                    shadowOpacity: 0.5,
+                    shadowRadius: 10,
+                },
+                android: {
+                    elevation: 20
+                }
+            }
+        )
+
+    }
+})
+
+export default Shadow;
