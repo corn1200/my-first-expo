4e72bae68548f9091385f804ab774e51ba0c4662
corn1200 <corn120033@gmail.com>
2021-12-23 13:53:02 +0900

    여러가지 press 관련 이벤트 지정 및 longPress 판정 시간 설정

--- src/MyButton.js
+++ src/MyButton.js
@@ -8,7 +8,11 @@ import PropTypes from 'prop-types';
 const MyButton = ({ title, onPress, children, title2 }) => {
     return (
         <TouchableOpacity
-            onPress={onPress}
+            onPress={() => console.log('press')}
+            onPressIn={() => console.log('in')}
+            onPressOut={() => console.log('out')}
+            onLongPress={() => console.log('long')}
+            delayLongPress={3000}
             pressRetentionOffset={{ bottom: 10, top: 10, left: 10, right: 10 }}
         >
             <View style={{ backgroundColor: 'red', padding: 10, margin: 10 }}>
