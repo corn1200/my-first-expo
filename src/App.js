cbbe24713f1fae89495ec2d8ea667d3e26cc1dba
corn1200 <corn120033@gmail.com>
2021-12-23 23:44:15 +0900

    ThemeProvider 를 이용하여 테마 설정 가능

--- src/App.js
+++ src/App.js
@@ -2,23 +2,37 @@
 import React from 'react';
 import { styles } from './style';
 import { StatusBar } from 'expo-status-bar';
-import styled, { css } from 'styled-components/native';
+import styled, { ThemeProvider } from 'styled-components/native';
 import Input from "./Input";
 
 const Container = styled.View`
     flex: 1;
-    background-color: #e3e3e3;
+    background-color: ${({ theme }) => theme.bgColor};
     align-items: center;
     justify-content: center;
 `;
 
+const lightTheme = {
+    inputColor: '#111',
+    inputBorder: '#111',
+    bgColor: '#e3e3e3'
+};
+
+const darkTheme = {
+    inputColor: '#e3e3e3',
+    inputBorder: '#e3e3e3',
+    bgColor: '#111'
+};
+
 // 화면에 띄워줄 컴포넌트 return()
 export default function App() {
     return (
-        <Container>
-            <StatusBar style='auto' />
-            <Input placeholder="Type a message..." />
-            <Input />
-        </Container>
+        <ThemeProvider theme={lightTheme}>
+            <Container>
+                <StatusBar style='auto' />
+                <Input placeholder="Type a message..." />
+                <Input />
+            </Container>
+        </ThemeProvider>
     );
 }
