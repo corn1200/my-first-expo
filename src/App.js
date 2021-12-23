2d123a8ba27d25024e20ecf6f8e8f417653b29ce
corn1200 <corn120033@gmail.com>
2021-12-23 14:10:03 +0900

    텍스트를 입력하여 값의 변화가 생길 때 마다 이벤트 객체의 값을 출력한다

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // 필요한 모듈({..., ...}) 불러오기(import)
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, TextInput } from 'react-native';
 import MyButton from './MyButton';
 import React, { useState } from 'react';
 
@@ -12,12 +12,18 @@ export default function App() {
     const [multiple, setMultiple] = useState(1);
     // onPress로 state를 수정하는 함수를 넘겨줌
     // state 가 변경 되어(set~) 화면이 리렌더링 됨
+    // 텍스트 입력기(TextInput)에 값 변화가 일어날 때(onChange) 마다
+    // 해당 변화 값(event)을 출력한다
     return (
         <View style={styles.container}>
-            <Text style={{ fontSize: 20 }}>{addition}</Text>
+            <TextInput
+                onChange={event => console.log(event.nativeEvent.text)}
+                style={{ borderWidth: 1, padding: 10, fontSize: 20 }}
+            />
+            {/* <Text style={{ fontSize: 20 }}>{addition}</Text>
             <Text style={{ fontSize: 20 }}>{multiple}</Text>
             <MyButton title="addition" onPress={() => setAddition(addition + 2)} />
-            <MyButton title="multiple" onPress={() => setMultiple(multiple * 2)} />
+            <MyButton title="multiple" onPress={() => setMultiple(multiple * 2)} /> */}
         </View>
     );
 }
