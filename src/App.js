d6223d69dba2c92550ca8e7c9a9d8286b3a52a05
corn1200 <corn120033@gmail.com>
2021-12-23 20:38:11 +0900

    특정한 스타일을 가지는 컴포넌트를 생성

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 // 필요한 모듈({..., ...}) 불러오기(import)
-import { View } from 'react-native';
 import React from 'react';
 import { styles } from './style';
 import { StatusBar } from 'expo-status-bar';
-import styled from 'styled-components';
+import styled from 'styled-components/native';
 
-const Container = styled.view`
+const Container = styled.View`
     flex: 1;
-    background-color: '#fff';
-    align-items: 'center';
-    justify-content: 'center';
+    background-color: #e3e3e3;
+    align-items: center;
+    justify-content: center;
 `;
+
+const StyledText = styled.Text`
+    font-size: 20px;
+    font-weight: 600;
+    color: blue;
+`;
+
+const StyledButton = styled.Button``;
 
 // 화면에 띄워줄 컴포넌트 return()
 export default function App() {
     return (
         <Container>
             <StatusBar style='auto' />
+            <StyledText style={{ color: 'red' }}>Styled components</StyledText>
+            <StyledButton title="Styled" onPress={() => alert("styled !!")} />
         </Container>
     );
 }
