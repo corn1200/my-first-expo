c1eabefe72bf64f33fa75e401fd8dc89241d6263
corn1200 <corn120033@gmail.com>
2021-12-23 12:22:17 +0900

    useState를 통해 함수 컴포넌트에서 state 변수 및 setter 함수 생성

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 // 필요한 모듈({..., ...}) 불러오기(import)
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 import MyButton from './MyButton';
+import React, { useState } from 'react';
 
 // 화면에 띄워줄 컴포넌트 return()
 export default function App() {
+    // 함수 컴포넌트(App())에서 state 변수(additio, multiple)와
+    // state 변수를 수정할 수 있는 setter 함수(set~)를 설정하고
+    // 초깃값을 지정함(useState(N))
+    const [addition, setAddition] = useState(0);
+    const [multiple, setMultiple] = useState(1);
     return (
         <View style={styles.container}>
+            <Text style={{ fontSize: 20 }}>{addition}</Text>
+            <Text style={{ fontSize: 20 }}>{multiple}</Text>
             <MyButton />
             <MyButton title="MyButton 2" onPress={() => alert("2")}>
                 MyButton3
